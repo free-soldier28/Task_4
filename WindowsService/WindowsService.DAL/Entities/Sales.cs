0de547ef5c4b2e6b3207634685bd2912022a91fc
romik280h <romik280h@gmail.com>
2017-12-05 23:56:52 +0300

    Create a layer Data Access Layer. created entities and connected EntityFramework

---
+++ WindowsService/WindowsService.DAL/Entities/Sales.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService.DAL.Entities
+{
+    public class Sales
+    {
+        public int Id { get; set; }
+        public int IdManager { get; set; }
+        public int IdCustomer { get; set; }
+        public int IdProduct { get; set; }
+        public DateTime Date { get; set; }
+
+        public virtual Manager Manager { get; set; }
+        public virtual Customer Customer { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
---
+++ WindowsService/WindowsService.DAL/EntityFramework/SalesContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsService.DAL.Entities;
+
+namespace WindowsService.DAL.EntityFramework
+{
+    public class SalesContext: DbContext
+    {
+        public DbSet<Manager> Managers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sales> Saleses { get; set; }
+
+
+        public SalesContext(string connectionsString) : base(connectionsString)
+        {
+            
+        }
+    }
+}
