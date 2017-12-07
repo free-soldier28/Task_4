b9246c4c041d6309013df31bf6db6959444825c5
romik280h <romik280h@gmail.com>
2017-12-07 23:57:24 +0300

    Created a layer of Business Logic Laer and intermediate entities

---
+++ WindowsService/WindowsService.BLL/DTO/ProductDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WindowsService.BLL.DTO
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ WindowsService/WindowsService.BLL/DTO/SalesDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WindowsService.BLL.DTO
+{
+    public class SalesDTO
+    {
+        public int Id { get; set; }
+        public int IdManager { get; set; }
+        public int IdCustomer { get; set; }
+        public int IdProduct { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
