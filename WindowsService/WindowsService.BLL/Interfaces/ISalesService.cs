d9b08f9e139bb8d1be35c5a97cdb9d867b655d58
roman <romik280h@gmail.com>
2017-12-11 15:34:52 +0300

    Implemented the save class for the parsed file

---
+++ WindowsService/WindowsService.BLL/Interfaces/ISalesService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService.BLL.Interfaces
+{
+    public interface ISalesService
+    {
+        void AddSales(string managerName, DateTime date, string[] substrings);
+    }
+}
--- WindowsService/WindowsService.BLL/Services/SalesService.cs
+++ WindowsService/WindowsService.BLL/Services/SalesService.cs
@@ -1,44 +1,92 @@
-﻿
-using System;
+﻿using System;
 using System.Linq;
 using WindowsService.BLL.DTO;
+using WindowsService.BLL.Interfaces;
 using WindowsService.DAL.Interfaces;
-using WindowsService.DAL.Repositories;
+using AutoMapper;
+using Entities;
 
 namespace WindowsService.BLL
 {
-    public class SalesService
+    public class SalesService: ISalesService
     {
         IUnitOfWork Database { get; set; }
 
-        public SalesService()
+        public SalesService(IUnitOfWork uow)
         {
-            //Database = uow;
+            Database = uow;
         }
 
         public void AddSales(string managerName, DateTime date, string[] substrings)
         {
-            ManagerRepository managerRepository = new ManagerRepository();
-            int idManager = managerRepository.Find(x => x.SecondName == managerName).Select(z => z.Id).FirstOrDefault();
+            int idManagerDTO = Database.Managers.Find(x => x.SecondName == managerName).Select(z => z.Id).FirstOrDefault();
+            int idCustomerDTO = Database.Customers.Find(x => x.FullName == substrings[1]).Select(z => z.Id).FirstOrDefault();
+            int idProductDTO = Database.Products.Find(x => x.Name == substrings[2]).Select(z => z.Id).FirstOrDefault();
 
-            CustomerRepository customerRepository = new CustomerRepository();
-            int idCustomer = customerRepository.Find(x => x.FullName == substrings[1]).Select(z => z.Id).FirstOrDefault();
+            if (idManagerDTO == 0)
+            {
+                ManagerDTO managerDto = new ManagerDTO()
+                {
+                    Id = 0,
+                    SecondName = managerName
+                };
 
-            ProductRepository productRepository = new ProductRepository();
-            int idProduct = productRepository.Find(x => x.Name = substrings[2]).Select(z => z.Id).FirstOrDefault();
+                Mapper.Initialize(cfg => cfg.CreateMap<ManagerDTO, Manager>());
+                var manager = Mapper.Map<ManagerDTO, Manager>(managerDto);
+                Database.Managers.Create(manager);
+                Database.Save();
+
+                idManagerDTO = Database.Managers.Find(x => x.SecondName == managerName).Select(z => z.Id).FirstOrDefault();
+            }
+
+            if (idCustomerDTO == 0)
+            {
+                CustomerDTO customerDto = new CustomerDTO()
+                {
+                    Id = 0,
+                    FullName = substrings[1]
+                };
+
+                Mapper.Initialize(cfg => cfg.CreateMap<CustomerDTO, Customer>());
+                var customer = Mapper.Map<CustomerDTO, Customer>(customerDto);
+                Database.Customers.Create(customer);
+                Database.Save();
+
+                idCustomerDTO = Database.Customers.Find(x => x.FullName == substrings[1]).Select(z => z.Id).FirstOrDefault();
+            }
+
+            if (idProductDTO == 0)
+            {
+                ProductDTO productDto = new ProductDTO()
+                {
+                    Id = 0,
+                    Name = substrings[2]
+                };
+
+                Mapper.Initialize(cfg => cfg.CreateMap<ProductDTO, Product>());
+                var product = Mapper.Map<ProductDTO, Product>(productDto);
+                Database.Products.Create(product);
+                Database.Save();
+
+                idProductDTO = Database.Products.Find(x => x.Name == substrings[2]).Select(z => z.Id).FirstOrDefault();
+            }
 
 
-            SalesRepository salesRepository = new SalesRepository();
             SalesDTO salesDto = new SalesDTO
             {
                 Id = 0,
-                IdManager = idManager,
-                IdCustomer = idCustomer,
-                IdProduct = idProduct,
+                IdManager = idManagerDTO,
+                IdCustomer = idCustomerDTO,
+                IdProduct = idProductDTO,
                 Date = Convert.ToDateTime(substrings[0]),
                 Amount = Convert.ToDouble(substrings[3])
             };
-            salesRepository.Create(salesDto);
+
+            Mapper.Initialize(cfg => cfg.CreateMap<SalesDTO, Sales>());
+            var sales = Mapper.Map<SalesDTO,Sales>(salesDto);
+
+            Database.Saleses.Create(sales);
+            Database.Save();
         }
     }
 }
